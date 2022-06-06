327cce1aa711dac52a347f31a0cfccac7c34930c
ikoshele <ivan.koshelev2012@gmail.com>
2022-06-06 19:33:56 +0200

    Notepad rings

--- public/template-parts/cards/notepad-card.php
+++ public/template-parts/cards/notepad-card.php
@@ -1,10 +1,6 @@
 <div class="notepad-card">
+    <div class="notepad-card__circles "></div>
     <img src="<?php echo $img_url ?>" alt="<?php echo $img_alt ?>" class="notepad-card__icon">
     <h5 class="notepad-card__title fz-28-20"><?php echo $title ?></h5>
     <p class="notepad-card__content"><?php echo $content ?></p>
-    <div class="notepad__rings">
-        <?php for ($i = 0; $i < 6; $i++) { ?>
-            <div class="ring"></div>
-        <?php } ?>
-    </div>
 </div>
--- public/template-parts/sections/hero-section.php
+++ public/template-parts/sections/hero-section.php
@@ -23,6 +23,9 @@
                     <p>Storytelling in business has<br>become a field in its own right<br>as industries have grown</p>
                 </div>
                 <div class="hero__cards">
+                    <svg class="clip-svg" aria-hidden="true">
+                        <clipPath id="notepad-card-clippath" clipPathUnits="objectBoundingBox"><path d="M0,0.045 C0,0.02,0.021,0,0.047,0 H0.953 C0.979,0,1,0.02,1,0.045 V0.134 H0.965 C0.946,0.134,0.93,0.149,0.93,0.168 C0.93,0.186,0.946,0.201,0.965,0.201 H1 V0.268 H0.965 C0.946,0.268,0.93,0.283,0.93,0.302 C0.93,0.32,0.946,0.335,0.965,0.335 H1 V0.402 H0.965 C0.946,0.402,0.93,0.417,0.93,0.436 C0.93,0.454,0.946,0.469,0.965,0.469 H1 V0.536 H0.965 C0.946,0.536,0.93,0.551,0.93,0.57 C0.93,0.588,0.946,0.603,0.965,0.603 H1 V0.67 H0.965 C0.946,0.67,0.93,0.685,0.93,0.704 C0.93,0.722,0.946,0.737,0.965,0.737 H1 V0.804 H0.965 C0.946,0.804,0.93,0.819,0.93,0.838 C0.93,0.856,0.946,0.871,0.965,0.871 H1 V0.955 C1,0.98,0.979,1,0.953,1 H0.047 C0.021,1,0,0.98,0,0.955 V0.045"></path></clipPath>
+                    </svg>
                     <?php
                     $img_url = 'image/notepad-cards/notepad-img1.svg';
                     $img_alt = 'img';
