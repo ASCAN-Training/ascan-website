7572ad2b4ce591d12fb13697376e12c272073829
ikoshele <ivan.koshelev2012@gmail.com>
2022-09-08 00:27:48 +0200

    mint restore

---
+++ public/mint.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ua">
+
+<?php include('template-parts/head.php') ?>
+
+
+<body>
+<canvas class="grad"></canvas>
+<div class="main-wrapper">
+    <?php include('template-parts/generic/header-simple.php') ?>
+
+    <main>
+        <?php include('template-parts/sections/generate-nft-section.php') ?>
+    </main>
+
+    <?php include('template-parts/generic/footer-simple.php') ?>
+
+</div>
+
+
+<?php include('template-parts/svg-container.php') ?>
+
+
+<script defer src="js/app.js" type="text/javascript"></script>
+
+</body>
+
+</html>
