af0402a5c10e5d6ccfd27403351b948df8a99518
ikoshele <ivan.koshelev2012@gmail.com>
2022-09-11 17:45:42 +0200

    Pull request: Styles for text pages

--- public/copyright-notice.php
+++ public/copyright-notice.php
@@ -6,16 +6,13 @@
 
 <body>
 <canvas class="grad"></canvas>
-<!--<div class="flying-light">
-    <span></span>
-</div>-->
 <div class="main-wrapper">
     <?php include('template-parts/generic/header.php') ?>
 
     <main>
         <section>
             <div class="container-main">
-                <div class="get-in-touch">
+                <div class="get-in-touch text-page">
                     <div class="get-in-touch__heading">
                         <h1 class="section-title title-animation">
                             <?php
@@ -26,27 +23,46 @@
                             Notice
                         </h1>
                     </div>
-                    <br>
-                    <div class="content">
-                        <br />
-                        <br />
+
+                    <div class="editor-content">
+
+
                         <p>© 2022 ASCAN Training. All rights reserved.</p>
-                        <br />
-                        <br />
-                        <h3>Statement/Declaration of Rights</h3><br />
-                        <p>Subject to the provisions of this notice, this website and all its content, information, or material is the copyright of ASCAN Training, together with its licensors. Accordingly, your use of our website or its services does not constitute any license to use the copyright on our website.</p>
-                        <br />
-                        <p>Except to the extent permitted by the applicable copyright law or ASCAN Training, any form of use, reproduction, or redistribution of part of all of the content, information, or material on this website in any form is hereby prohibited.</p>
-                        <br />
-                        <p>You may not, except otherwise with prior permission and express written consent by the ASCAN Training, copy, download, print, extract, exploit, adapt, edit, modify, republish, reproduce, rebroadcast, duplicate, distribute, or publicly display any of the content, information, or material on this website for non-personal or commercial purposes, except for any other use as permitted by the applicable copyright law.</p>
-                        <br />
-                        <p>You may also not transmit, host, or store any such content, information, or material in any form or through any means, including but not limited to photocopying, recording, or in any print, electronic or digital form, or on any other website.</p>
-                        <br />
-                        <p>However, you are permitted to copy the content, information, or material on this website for personal use, educational use of individual third parties, government use, or any other use permitted under the applicable copyright law while acknowledging this author ASCAN Training as the source of any such content, information, or material.</p>
-                        <br />
-                        <p>Subject to our terms &amp; conditions, you are not permitted to post information or add content copyrighted by third parties. Pursuant to the Digital Millennium Copyright Act (DMCA), ASCAN Training will respond to takedown notices or abuse reports by copyright owners to remove any abusive or infringing content on this website.</p>
-                        <br />
-                        <p>ASCAN Training will not be responsible for the quality, accuracy, completeness, or appropriateness of the content, information, or material on this website.<br/>email: <a href="mailto:contact@ascantraining.com">contact@ascantraining.com</a></p>
+
+
+                        <h3>Statement/Declaration of Rights</h3>
+                        <p>Subject to the provisions of this notice, this website and all its content, information, or
+                            material is the copyright of ASCAN Training, together with its licensors. Accordingly, your
+                            use of our website or its services does not constitute any license to use the copyright on
+                            our website.</p>
+
+                        <p>Except to the extent permitted by the applicable copyright law or ASCAN Training, any form of
+                            use, reproduction, or redistribution of part of all of the content, information, or material
+                            on this website in any form is hereby prohibited.</p>
+
+                        <p>You may not, except otherwise with prior permission and express written consent by the ASCAN
+                            Training, copy, download, print, extract, exploit, adapt, edit, modify, republish,
+                            reproduce, rebroadcast, duplicate, distribute, or publicly display any of the content,
+                            information, or material on this website for non-personal or commercial purposes, except for
+                            any other use as permitted by the applicable copyright law.</p>
+
+                        <p>You may also not transmit, host, or store any such content, information, or material in any
+                            form or through any means, including but not limited to photocopying, recording, or in any
+                            print, electronic or digital form, or on any other website.</p>
+
+                        <p>However, you are permitted to copy the content, information, or material on this website for
+                            personal use, educational use of individual third parties, government use, or any other use
+                            permitted under the applicable copyright law while acknowledging this author ASCAN Training
+                            as the source of any such content, information, or material.</p>
+
+                        <p>Subject to our terms &amp; conditions, you are not permitted to post information or add
+                            content copyrighted by third parties. Pursuant to the Digital Millennium Copyright Act
+                            (DMCA), ASCAN Training will respond to takedown notices or abuse reports by copyright owners
+                            to remove any abusive or infringing content on this website.</p>
+
+                        <p>ASCAN Training will not be responsible for the quality, accuracy, completeness, or
+                            appropriateness of the content, information, or material on this website.<br/>email: <a
+                                    href="mailto:contact@ascantraining.com">contact@ascantraining.com</a></p>
                     </div>
 
                 </div>
